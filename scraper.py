0c8916fbc6f50cea0ac78b5e5b3f9112ac2475d9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-29 10:37:35 -0500

    document code

--- scraper.py
+++ scraper.py
@@ -5,38 +5,58 @@ URL = 'https://www.elespectador.com/noticias/'
 
 
 def run():
+
+    # --> Use of requests
+
     try:
         response = requests.get(URL)
-        # print(response.status_code)
-        # print(response.headers)
-        # print(response.request.headers)
-        # print(response.text)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            # print(type(soup))
-
-            today = soup.find('div', attrs={
-                'class': 'date'
-            }).get_text()
-            print(today)
-
-            titles_div = soup.find_all('div', attrs={
-                'class': 'node-title'
-            })
-            titles = [titles_div.find('a').get_text() for titles_div in titles_div]
-            print(titles)
-
-            with open('result.txt', 'w', encoding='utf-8') as f:
-                for title in titles:
-                    f.write(title)
-                    f.write('\n')
-
-        else:
-            print('Error:', response.status_code)
-
     except e:
         print('Error', e)
+        
+    # Status code
+    # print(response.status_code)
+
+    # Headers
+    # print(response.headers)
+
+    # Encoding
+    # print(response.encoding)
+
+    # Body
+    # print(response.text)
+
+    # Request headers
+    # print(response.request.headers)
+
+    # Request methods
+    # print(response.request.method)
+
+    # --> Use of BeautifulSoup
+
+
+    # Creating a soup
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # print(type(soup))
+
+        today = soup.find('div', attrs={
+            'class': 'date'
+        }).get_text()
+        print(today)
+
+        titles_div = soup.find_all('div', attrs={
+            'class': 'node-title'
+        })
+        titles = [titles_div.find('a').get_text() for titles_div in titles_div]
+        print(titles)
+
+        with open('result.txt', 'w', encoding='utf-8') as f:
+            for title in titles:
+                f.write(title)
+                f.write('\n')
+
+    else:
+        print('Error:', response.status_code)
 
 
 if __name__ == '__main__':
